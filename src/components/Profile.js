3a07b6132633bd26cf15a0c7a5542ee297511800
NAZMEL <melnyknasar@gmail.com>
2022-01-12 13:51:20 +0200

    Initialize project

---
+++ src/components/Profile.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Profile = () =>{
+    return(
+      <div className='content'>
+      <div>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png" alt=""/>
+      </div>
+
+      <div>
+
+      </div>
+
+      <div>
+        <div>
+          new post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post2
+          </div>
+        </div>
+      </div>
+    </div>
+    )
+}
+
+export default Profile;
