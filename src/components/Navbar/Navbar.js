05772980f979a2820053cc788a8cd0a30da6ff1c
NAZMEL <melnyknasar@gmail.com>
2022-01-20 11:50:43 +0200

    Add diaologs component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,19 +6,19 @@ const Navbar = () =>{
     return(
         <nav className={navbarStyle.nav}>
         <div className={navbarStyle.item}>
-          <a href="">Profile</a>
+          <a href="/profile">Profile</a>
         </div>
         <div className={navbarStyle.item}>
-          <a href="">Messages</a>
+          <a href="/dialogs">Messages</a>
         </div>
         <div className={navbarStyle.item}>
-          <a href="">News</a>
+          <a href="/news">News</a>
         </div>
         <div className={navbarStyle.item}>
-          <a href="">Music</a>
+          <a href="/music">Music</a>
         </div>
         <div className={navbarStyle.item}>
-          <a href="">Settings</a>
+          <a href="/settings">Settings</a>
         </div>
       </nav>
     )
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,7 +5,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () =>{
     return(
-      <div className={profileStyle.content}>
+      <div>
       <div>
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png" alt=""/>
       </div>
