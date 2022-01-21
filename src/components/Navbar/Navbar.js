e043fc6c7f333c80d4dde486324583748e99cd70
NAZMEL <melnyknasar@gmail.com>
2022-01-21 13:30:17 +0200

    Add links for simple page

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import navbarStyle from './Navbar.module.css';
 
 
@@ -6,19 +7,19 @@ const Navbar = () =>{
     return(
         <nav className={navbarStyle.nav}>
         <div className={navbarStyle.item}>
-          <a href="/profile">Profile</a>
+          <NavLink to="/profile">Profile</NavLink>
         </div>
         <div className={navbarStyle.item}>
-          <a href="/dialogs">Messages</a>
+          <NavLink to="/dialogs">Messages</NavLink>
         </div>
         <div className={navbarStyle.item}>
-          <a href="/news">News</a>
+          <NavLink to="/news">News</NavLink>
         </div>
         <div className={navbarStyle.item}>
-          <a href="/music">Music</a>
+          <NavLink to="/music">Music</NavLink>
         </div>
         <div className={navbarStyle.item}>
-          <a href="/settings">Settings</a>
+          <NavLink to="/settings">Settings</NavLink>
         </div>
       </nav>
     )
