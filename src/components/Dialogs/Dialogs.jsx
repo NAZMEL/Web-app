157e7091daf90d5f5c37848368d8c477a78486ca
NAZMEL <melnyknasar@gmail.com>
2022-01-21 14:21:05 +0200

    Add dilog components

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,34 @@ import dialogsStyle from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return(
-        <div>
-            Dialogs
+        <div className={dialogsStyle.dialogs}>
+            <div className={dialogsStyle.dialogsItems}>
+               
+                <div className={dialogsStyle.dialog + ' '+ dialogsStyle.active}>
+                    Nazar
+                </div>
+                <div className={dialogsStyle.dialog}>
+                    Maria
+                </div>
+                <div className={dialogsStyle.dialog}>
+                    Vlad
+                </div>
+                <div className={dialogsStyle.dialog}>
+                    Vad
+                </div>
+            </div>
+
+            <div className={dialogsStyle.dialogMessages}>
+                <div className={dialogsStyle.message}>
+                    message1
+                </div>
+                <div className={dialogsStyle.message}>
+                    message2
+                </div>
+                <div className={dialogsStyle.message}>
+                    message2
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,19 +7,29 @@ const Navbar = () =>{
     return(
         <nav className={navbarStyle.nav}>
         <div className={navbarStyle.item}>
-          <NavLink to="/profile">Profile</NavLink>
+          <NavLink to="/profile" className={
+            navData => navData.isActive ? navbarStyle.active : navbarStyle.item
+          } >Profile</NavLink>
         </div>
         <div className={navbarStyle.item}>
-          <NavLink to="/dialogs">Messages</NavLink>
+          <NavLink to="/dialogs" className={
+            navData => navData.isActive ? navbarStyle.active : navbarStyle.item
+          }>Messages</NavLink>
         </div>
         <div className={navbarStyle.item}>
-          <NavLink to="/news">News</NavLink>
+          <NavLink to="/news" className={
+            navData => navData.isActive ? navbarStyle.active : navbarStyle.item
+          }>News</NavLink>
         </div>
         <div className={navbarStyle.item}>
-          <NavLink to="/music">Music</NavLink>
+          <NavLink to="/music" className={
+            navData => navData.isActive ? navbarStyle.active : navbarStyle.item
+          }>Music</NavLink>
         </div>
         <div className={navbarStyle.item}>
-          <NavLink to="/settings">Settings</NavLink>
+          <NavLink to="/settings"  className={
+            navData => navData.isActive ? navbarStyle.active : navbarStyle.item
+          } >Settings</NavLink>
         </div>
       </nav>
     )
