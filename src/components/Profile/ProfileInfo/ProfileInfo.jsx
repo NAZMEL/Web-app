b60cc01b1f1ff61d0e0ccecc50a65561a13e7868
NAZMEL <melnyknasar@gmail.com>
2022-02-10 10:45:37 +0200

    Get all info about users

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import Preloader from "../../common/Preloader/Preloader";
+import UserIconContainer from "../../common/UserIcon/UserIconContainer";
 import style from './ProfileInfo.module.css';
 
 const ProfileInfo = (props) =>{
@@ -11,15 +13,46 @@ const ProfileInfo = (props) =>{
 
     return(
       <div>
-        <div>
+        {/* <div>
           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png" alt=""/>
-        </div>
+        </div> */}
 
-        <div className={style.descriptionBlock}>
-          <img src={props.profile.photos.small} />
-          <div>
-            description
+        <div className={style.userProfile}>
+          <div className={style.profileSidebar}>
+            <div className={style.userIdBlock}>User ID: {props.profile.userId}</div>
+            <UserIconContainer imgPath={props.profile.photos.small}/>
+            <div className={style.aboutMeBlock}>
+              <span className={style.headSpan}>Description: </span><br/> {props.profile.aboutMe}
+            </div>
           </div>
+
+          <div className={style.profileContacts}>
+            <div className={style.profileContactsItem}>
+              <span className={style.headSpan}>Facebook: </span>
+              <NavLink to={props.profile.contacts.facebook != null
+                            ? props.profile.contacts.facebook
+                            : '/'}>{props.profile.contacts.facebook}</NavLink>
+            </div>
+            <div className={style.profileContactsItem}>
+              <span className={style.headSpan}>Twitter: </span>
+              <NavLink to={props.profile.contacts.twitter != null
+                            ? props.profile.contacts.twitter
+                            : '/'}>{props.profile.contacts.twitter}</NavLink>
+            </div>
+            <div className={style.profileContactsItem}>
+              <span className={style.headSpan}>Instagram: </span>
+              <NavLink to={props.profile.contacts.instagram != null 
+                            ? props.profile.contacts.instagram
+                            : '/'}>{props.profile.contacts.instagram}</NavLink>
+            </div>
+            <div className={style.profileContactsItem}>
+              <span className={style.headSpan}>You Tube: </span>
+              <NavLink to={props.profile.contacts.youtube != null 
+                            ? props.profile.contacts.youtube 
+                            : '/'}>{props.profile.contacts.youtube}</NavLink>
+            </div>
+          </div>
+          
         </div>
     </div>
     )
